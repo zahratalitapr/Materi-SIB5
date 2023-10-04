c594b90c7107cfab3d4d9a2f2551587cadc0c7ef
zahratalitapr <zahratalitapr12@gmail.com>
2023-10-04 16:31:47 +0700

    Tugas php 7 nambahin dropdown information

---
+++ Tugas18_ZahraTalita_FDW1_PHP7/index.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once 'top.php';
+include_once 'menu.php';
+?>
+
+<div>
+    <div class="container-fluid px-4">
+        <!-- <h1>Selamat Datang di Halaman Admin</h1> -->
+
+        <?php
+        // <!-- membuat logic sederhana untuk mengarahkan url ke file -->
+        // yang mempunyai extension php
+
+        // $url = $_GET['url'];
+        $url = !isset($_GET['url']) ? 'dashboard' : $_GET['url'];
+        if ($url == 'dashboard') {
+            include_once 'dashboard.php';
+        } else if (!empty($url)){
+            include_once ''.$url.'.php';
+        } else {
+            'dashboard.php';
+        }
+        ?>
+    </div>
+</div>
+
+<?php
+include_once 'bottom.php';
+?>
