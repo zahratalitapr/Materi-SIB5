23ac0c8ac0ee490b8943045018071e79533f9d33
zahratalitapr <zahratalitapr12@gmail.com>
2023-11-01 10:47:03 +0700

    latihan laravel 1

---
+++ resources/views/coba/daftar.blade.php
@@ -0,0 +1,38 @@
+@php
+
+$no = 1;
+
+$s1 = ['nama'=> 'Fawwaz', 'nilai'=>85];
+$s2 = ['nama'=> 'Ali', 'nilai'=>95];
+$s3 = ['nama'=> 'Ari', 'nilai'=>75];
+$s4 = ['nama'=> 'Andi', 'nilai'=>65];
+$s5 = ['nama'=> 'Aji', 'nilai'=>55];
+
+$judul = ['No', 'Nama', 'Nilai', 'Keterangan'];
+
+$siswa = [$s1, $s2, $s3, $s4, $s5];
+
+@endphp
+
+<table border="1" align="center" cellpadding="10">
+    <thead>
+        <tr>
+            @foreach($judul as $j)
+            <th>{{$j}}</th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            @foreach($siswa as $s)
+            @php
+            $ket = ($s['nilai'] >= 60) ? 'Lulus' : 'Gagal';
+            @endphp
+            <td>{{$no++}}</td>
+            <td>{{$s['nama']}}</td>
+            <td>{{$s['nilai']}}</td>
+            <td>{{$ket}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/salam', function(){
+    return "Assalamualaikum Selamat belajar Laravel";
+});
+
+// tambah routing dengan parameter
+Route::get('/staff/{nama}/{divisi}', function($nama, $divisi){
+    return 'Nama Pegawai : '.$nama. '<br> Departemen : '.$divisi;
+});
+
+// routing dengan memanggil nama file dari view
+Route::get('/kondisi', function(){
+    return view('kondisi');
+});
+Route::get('/nilai', function(){
+    return view('coba.nilai');
+});
+Route::get('/daftarnilai', function(){
+    return view('coba.daftar');
+});
