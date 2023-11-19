24426ffc9859dfc40dccc62834563cd472f04518
zahratalitapr <zahratalitapr12@gmail.com>
2023-11-19 20:59:37 +0700

    namabahin heading di excel export

--- app/Exports/ProdukExport.php
+++ app/Exports/ProdukExport.php
@@ -7,7 +7,7 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 
-class ProdukExport implements FromCollection
+class ProdukExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
