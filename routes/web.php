5a0d82ffb5cf665798883c0aab9483e5519ab5f3
zahratalitapr <zahratalitapr12@gmail.com>
2023-11-22 09:25:29 +0700

    instalasi node n npm

--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,6 @@ Route::post('/produk/import/', [ProdukController::class, 'importProduk']);
 Route::resource('pelanggan', PelangganController::class);
 
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
